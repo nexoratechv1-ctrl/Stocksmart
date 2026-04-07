8031f943ede59a1a222868d03a71760bce8044d2
nexoratechv1-ctrl <nexoratech.v1@gmail.com>
2026-04-08 00:57:21 +0300

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = 'stocksmart-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/style.css',
+  '/static/js/app.js',
+  '/manifest.json',
+  '/login',
+  '/register'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
